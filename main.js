2fde5fedc3698840449419a408d07ce7d71d96d5
gitthathub403 <117359970+gitthathub403@users.noreply.github.com>
2023-09-23 22:13:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
 }
 
 
